537351d10cf76f7dd97badee57adc3cfa47d13d6
elenalev2686 <elenalev2686@gmail.com>
2023-08-03 16:45:34 +0700

    changed ignore

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 200; // начальный счет;
+        int addamount = 1320; // сумма пополнения счета;
+        int bonus = 1; // 1 бонусный рубль за каждые 100 рублей;
+        if (addamount > 1000) {
+            System.out.println("Итоговый счет:");
+            System.out.println((amount + addamount + ((addamount - 1000) / 100)) + " рублей");
+
+            System.out.println("Количество бонусных рублей:");
+            System.out.println(((addamount - 1000) / 100) + " бонусных рубля(ей)");
+        } else {
+            System.out.println("Итоговый счет:");
+            System.out.println((amount + addamount + " рублей"));
+            System.out.println("Бонусы не начисляются:");
+        }
+    }
+
+
+}
