7ca68101ab8efe082b2b4383f7814719f1c59313
elenalev2686 <elenalev2686@gmail.com>
2023-08-05 21:03:41 +0700

    correction2

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 200; // начальный счет;
-        int addamount = 900; // сумма пополнения счета;
+        int addamount = 1630; // сумма пополнения счета;
         int bonus = 1; // 1 бонусный рубль за каждые 100 рублей;
         if (addamount > 1000) {
             System.out.println("Итоговый счет:");
-            System.out.println((amount + addamount + ((addamount - 1000) / 100)) + " рублей");
+            System.out.println(amount + addamount + addamount  / 100 + " рублей");
 
             System.out.println("Количество бонусных рублей:");
-            System.out.println(((addamount - 1000) / 100) + " бонусных рубля(ей)");
+            System.out.println((addamount / 100) + " бонусных рубля(ей)");
         } else {
             System.out.println("Итоговый счет:");
             System.out.println((amount + addamount + " рублей"));
             System.out.println("Бонусы не начисляются:");
-            // Итоговый счет: 1100 //
-            // Бонусы не начисляются //
+            // Итоговый счет: 1826 //
+            // Количество бонусных рублей: 16 //
 
         }
     }
