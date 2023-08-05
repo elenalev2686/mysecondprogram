fc285da332410b674a902ba6454a369aaccf53ea
elenalev2686 <elenalev2686@gmail.com>
2023-08-05 20:34:38 +0700

    correction

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 200; // начальный счет;
-        int addamount = 1320; // сумма пополнения счета;
+        int addamount = 1620; // сумма пополнения счета;
         int bonus = 1; // 1 бонусный рубль за каждые 100 рублей;
         if (addamount > 1000) {
             System.out.println("Итоговый счет:");
-            System.out.println((amount + addamount + ((addamount - 1000) / 100)) + " рублей");
+            System.out.println(amount + addamount + (addamount / 100) + " рублей");
 
             System.out.println("Количество бонусных рублей:");
-            System.out.println(((addamount - 1000) / 100) + " бонусных рубля(ей)");
+            System.out.println((addamount / 100) + " бонусных рубля(ей)");
         } else {
             System.out.println("Итоговый счет:");
             System.out.println((amount + addamount + " рублей"));
